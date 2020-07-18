c599f89c34f435b7a71cec88d5ce5a2c251a127f
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-07-18 10:09:53 +0600

    add sample books data

---
+++ read-ing/Resources/sampleBookData.swift
@@ -0,0 +1,15 @@
+let book0 = Book(
+    id: 0,
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoyevsky",
+    pageCount: "1000"
+)
+
+let book1 = Book(
+    id: 1,
+    title: "Shoe Dog: A Memoir by the Creator of NIKE",
+    author: "Phil Knight",
+    pageCount: "400"
+)
+
+let sampleBookList = [book0, book1]
