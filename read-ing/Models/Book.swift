670521992b5a27308bc14752b2005c695f44610a
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-07-18 23:58:40 +0600

    add subtitle, image, isEbook

--- read-ing/Models/Book.swift
+++ read-ing/Models/Book.swift
@@ -1,8 +1,18 @@
 import Foundation
+import SwiftUI
 
 struct Book: Identifiable {
     let id: Int
     var title: String
-    var author: String
-    var pageCount: String
+    var subtitle: String?
+    var authors: [String]
+    var pageCount: Int
+    var image: Image?
+    var isEbook = false
 }
+
+//types of images
+//Image
+//CGImage
+//UIImage
+//CIImage
