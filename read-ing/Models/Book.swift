fe088e82d55f8df80e6f0ff6f7879f0ee494623e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-07-18 10:07:02 +0600

    adopt identifiable protocol

--- read-ing/Models/Book.swift
+++ read-ing/Models/Book.swift
@@ -1,14 +1,7 @@
-//
-//  File.swift
-//  read-ing
-//
-//  Created by Viktor Khotimchenko on 2020-07-16.
-//  Copyright © 2020 mrv1k. All rights reserved.
-//
-
 import Foundation
 
-struct Book {
+struct Book: Identifiable {
+    let id: Int
     var title: String
     var author: String
     var pageCount: String
