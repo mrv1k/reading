e13104db46552099da24a2710ea1857f5746da04
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-07-19 11:51:59 +0600

    basic implementation

--- read-ing/BookDetail.swift
+++ read-ing/BookDetail.swift
@@ -1,13 +1,32 @@
 import SwiftUI
 
 struct BookDetail: View {
+    let book: Book
+
     var body: some View {
-        Text("BookDetail coming soon")
+        VStack {
+            if book.image != nil {
+                Image(book.image!)
+            }
+
+            Text(book.title)
+
+            if book.subtitle != nil {
+                Text(book.subtitle!)
+            }
+
+            Text("by " + book.authors.joined(separator: ", "))
+
+            Text(String(book.pageCount) + " pages")
+
+//            Text("isEbook \(book.isEbook ? "true" : "false")")
+        }
     }
 }
 
 struct BookDetail_Previews: PreviewProvider {
     static var previews: some View {
-        BookDetail()
+        BookDetail(book: sampleBookWith["everything"]!)
+//        BookDetail(book: sampleBookWith["2_authors"]!)
     }
 }
