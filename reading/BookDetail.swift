63280fd2d770eb97167390bd8b5b123da4505ab7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-07-29 15:43:48 +0600

    update to display CoreData model

--- reading/BookDetail.swift
+++ reading/BookDetail.swift
@@ -1,13 +1,15 @@
 import SwiftUI
 
 struct BookDetail: View {
+    @Environment(\.managedObjectContext) private var managedObjectContext
     @Environment(\.presentationMode) private var isActive
     let book: Book
 
     var body: some View {
         VStack {
             if book.image != nil {
-                Image(book.image!)
+                Text("image")
+                // Image(book.image!)
             }
 
             Text(book.title)
@@ -16,14 +18,15 @@ struct BookDetail: View {
                 Text(book.subtitle!)
             }
 
-            Text("by " + book.authors.joined(separator: ", "))
+            Text("by " + book.authors)
+            // Text("by " + book.authors.joined(separator: ", "))
 
             Text(String(book.pageCount) + " pages")
 
             Button(action: {
                 self.isActive.wrappedValue.dismiss()
             }) {
-                Text("OK")
+                Text("Up to BookList")
             }
 
 //            Text("isEbook \(book.isEbook ? "true" : "false")")
@@ -33,15 +36,17 @@ struct BookDetail: View {
 
 struct BookDetail_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            BookDetail(
-                book: sampleBookWith["everything"]!
-            )
-
-            PreviewWithNavigation(
-                anyView: AnyView(BookDetail(book: sampleBookWith["everything"]!))
-            )
-        }
-        .environmentObject(UserData())
+        return BookDetail(book: SeedData.shared.book1())
+            .environment(\.managedObjectContext, SeedData.shared.context)
+        // Group {
+        //     BookDetail(
+        //         book: sampleBookWith["everything"]!
+        //     )
+        //
+        //     PreviewWithNavigation(
+        //         anyView: AnyView(BookDetail(book: sampleBookWith["everything"]!))
+        //     )
+        // }
+        // .environment(\.managedObjectContext, context)
     }
 }
