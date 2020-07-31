bdcaa340655a3b0ca262f7941cb3505cdc7a1b70
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-07-31 18:20:19 +0600

    make save optional, clean up generators

--- reading/Resources/SeedData.swift
+++ reading/Resources/SeedData.swift
@@ -3,6 +3,10 @@ import SwiftUI
 import CoreData
 
 class SeedData {
+    enum BookData: CaseIterable {
+        case minimum, everything, subtitle
+    }
+
     static let shared = SeedData()
     let moc: NSManagedObjectContext
 
@@ -13,59 +17,49 @@ class SeedData {
         self.moc = moc
     }
 
-    func book1() -> Book {
-        let book = Book(context: moc)
-        book.id = UUID()
+    private func minimum(book: Book) {
         book.title = "Crime and Punishment"
         book.authors = "Fyodor Dostoyevsky"
-        book.pageCount = Int16(1000)
-        do {
-            try moc.save()
-        } catch {
-            print(error)
-        }
-        return book
+        book.pageCount = Int16(449)
     }
 
     // "Shoe Dog: A Memoir by the Creator of NIKE"
-    func book2() -> Book {
-        let book = Book(context: moc)
-        book.id = UUID()
+    private func everything(book: Book) {
         book.title = "Shoe Dog"
         book.subtitle = "A Memoir by the Creator of NIKE"
         book.authors = "Phil Knight"
         book.pageCount = Int16(400)
-        do {
-            try moc.save()
-        } catch {
-            print(error)
+    }
+
+    private func subtitle(book: Book) {
+        book.title = "The Swift Programming Language"
+        book.subtitle = "(Swift 5.2 Edition)"
+        book.authors = "Apple Inc., Elppa Cin."
+        book.pageCount = Int16(500)
+    }
+
+    func makeBook(with data: BookData, save: Bool = false) -> Book {
+        let book = Book(context: moc)
+        book.id = UUID()
+
+        switch data {
+        case .minimum:
+            minimum(book: book)
+        case .everything:
+            everything(book: book)
+        case .subtitle:
+            subtitle(book: book)
+        }
+
+        if save {
+            do {
+                try moc.save()
+            } catch {
+                print(error)
+            }
         }
         return book
     }
-}
 
-// let book2 = Book(
-//     id: 2,
-//     title: "The Swift Programming Language",
-//     subtitle: "(Swift 5.2 Edition)",
-//     authors: ["Apple Inc.", "Another Author"],
-//     pageCount: 500,
-//     image: "swift_book_cover",
-//     isEbook: true
-// )
-//
-// let book3 = Book(
-//     id: 3,
-//     title: "Good Omens",
-//     authors: ["Neil Gaiman", "Terry Pratchett"],
-//     pageCount: 288
-// )
-//
-// let sampleBookArray = [book0, book1, book2, book3]
-//
-// let sampleBookWith = [
-//     "minimum": book0,
-//     "subtitle": book1,
-//     "everything": book2,
-//     "2_authors": book3,
-// ]
+    // TODO make seed book list
+}
