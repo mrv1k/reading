d58e420cf4ad942581311468dd059ac8beb053ec
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-08-01 18:30:02 +0600

    Change layout and fonts

--- reading/SupportingViews/BookRow.swift
+++ reading/SupportingViews/BookRow.swift
@@ -4,19 +4,31 @@ struct BookRow: View {
     var book: Book
 
     var body: some View {
-        HStack {
+        // Image("swift_book_cover")
+        //     .resizable()
+        //     .border(Color.black)
+        //     .frame(width: 70, height: 105)
+        VStack(alignment: .leading) {
             Text(book.title)
+                .font(.headline)
+                // .fontWeight(.semibold)
                 .lineLimit(1)
-            Spacer()
             Text(book.authors)
-            Spacer()
-            Text(String(book.pageCount))
-        }.multilineTextAlignment(.leading)
+                .font(.subheadline)
+                .fontWeight(.light)
+            Text("Page count: \(book.pageCount)")
+                .font(.footnote)
+
+        }
+        .multilineTextAlignment(.leading)
     }
 }
 
 struct BookRow_Previews: PreviewProvider {
     static var previews: some View {
-        BookRow(book: SeedData.shared.makeBook(with: .minimum))
+        Group {
+            BookRow(book: SeedData.shared.makeBook(with: .everything))
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
