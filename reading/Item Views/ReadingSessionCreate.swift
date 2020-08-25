3101f7ea1b1816643e84dc8721009a5a717ad7f7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-08-25 21:24:50 +0600

    add text fields for pages

--- reading/Item Views/ReadingSessionCreate.swift
+++ reading/Item Views/ReadingSessionCreate.swift
@@ -10,10 +10,29 @@ import SwiftUI
 
 struct ReadingSessionCreate: View {
     @Environment(\.managedObjectContext) private var viewContext
-    var book: Book
-    @State var selectedDate = Date()
 
-    var dateClosedRange: ClosedRange<Date> {
+    var book: Book
+
+    @State private var selectedDate = Date()
+    @State private var pageStartField = ""
+    @State private var pageEndField = ""
+    @State private var pagesReadField = ""
+    @State private var selection = ""
+
+    var pageStart: Int {
+        Int(pageStartField) ?? 0
+    }
+    var pageEnd: Int {
+        Int(pageEndField) ?? 0
+    }
+    var pagesRead: Int {
+        if pageEnd == 0 || pageStart == 0 {
+            return 0
+        }
+        return pageEnd - pageStart
+    }
+
+    private var dateClosedRange: ClosedRange<Date> {
         let min = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
         let max = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
         return min...max
@@ -21,25 +40,50 @@ struct ReadingSessionCreate: View {
 
     var body: some View {
         Form {
-            Section(header: Text("Book")) {
-                Text("\(book.title) by \(book.authors)")
+            Section {
+                BookRow(book: book)
             }
 
             DatePicker(
                 selection: $selectedDate,
                 in: dateClosedRange,
                 displayedComponents: .date,
-                label: { Text("Session Date") }
+                label: { Text("Date") }
             )
 
-            Text("pageStart")
-            Text("pageEnd")
-            Text("computed pagesRead")
+            Section {
+                HStack {
+                    TextField("Start page", text: $pageStartField)
+                        .keyboardType(.numberPad)
+                    TextField("End page", text: $pageEndField)
+                        .keyboardType(.numberPad)
+                }
+
+                HStack {
+                    TextField("Pages read", text: $pagesReadField)
+
+                    Picker("Preset", selection: $selection) {
+                        Text("10").tag("10")
+                        Text("15").tag("15")
+                        Text("20").tag("20")
+                        Text("25").tag("25")
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                }
+            }
 
             Text("timerStart?")
             Text("timerEnd?")
+
+            Section {
+                Button("Save") {
+                    print("wip")
+                }
+                .frame(maxWidth: .infinity)
+            }
+
         }
-        .navigationBarTitle(Text("Add reading sessions"))
+        .navigationBarTitle(Text("Add a sessions"))
     }
 }
 
@@ -49,9 +93,14 @@ struct ReadingSessionCreate_Previews: PreviewProvider {
 
         let book = BookSeeder(context: viewContext).insert(bookWith: .minimum)
 
-        return NavigationView {
+        return Group {
             ReadingSessionCreate(book: book)
-                .environment(\.managedObjectContext, viewContext)
+
+            NavigationView {
+                ReadingSessionCreate(book: book)
+                    .navigationBarTitleDisplayMode(.inline)
+            }
         }
+        .environment(\.managedObjectContext, viewContext)
     }
 }
