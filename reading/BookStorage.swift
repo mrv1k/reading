fe5c3e7ff9e47a2bf5170f2fea21414fef1de9b7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-09-02 16:22:03 +0600

    implement NSFetchedResultsController
    based on: https://www.donnywals.com/fetching-objects-from-core-data-in-a-swiftui-proje
    that's what I've been trying to achive with @FetchRequest wrapper
    I don't want my view to know about CoreData

---
+++ reading/BookStorage.swift
@@ -0,0 +1,46 @@
+//
+//  BookStorage.swift
+//  reading
+//
+//  Created by Viktor Khotimchenko on 2020-09-02.
+//  Copyright © 2020 mrv1k. All rights reserved.
+//  based on: https://www.donnywals.com/fetching-objects-from-core-data-in-a-swiftui-proje
+//
+
+import Foundation
+import Combine
+import CoreData
+
+class BookStorage: NSObject, ObservableObject {
+    @Published var books: [Book] = []
+    private let booksController: NSFetchedResultsController<Book>
+
+    init(viewContext: NSManagedObjectContext, sort: NSSortDescriptor) {
+        let fetchRequest: NSFetchRequest<Book> = Book.fetchRequest()
+        fetchRequest.sortDescriptors = [sort]
+
+        booksController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: viewContext,
+            sectionNameKeyPath: nil, cacheName: nil)
+
+        super.init()
+
+        booksController.delegate = self
+
+        do {
+            try booksController.performFetch()
+            books = booksController.fetchedObjects ?? []
+        } catch {
+            print(#function, "failed to fetch books")
+        }
+    }
+}
+
+extension BookStorage: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        guard let refetchedBooks = controller.fetchedObjects as? [Book]
+        else { return }
+        books = refetchedBooks
+    }
+}
