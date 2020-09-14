f66b44c113641b136fe1be6674b46f0c8b7ee037
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-09-14 13:32:24 +0600

    fix and refactor ViewModel
    fix misleading end validator
    add numeric end validator
    update to use numeric validation
    move inside enum

--- reading/Item Views/SessionCreatePagesSection.swift
+++ reading/Item Views/SessionCreatePagesSection.swift
@@ -18,36 +18,38 @@ class SessionCreatePagesViewModel: ObservableObject {
     var progress: Int { Int(progressField) ?? 0 }
 
     var startIsValid: Bool { start > 0 }
-    var endIsValid: Bool { end > start }
+    var endIsValid: Bool { end > 0 }
     var progressIsValid: Bool { progress > 0 }
+
+    var endIsAfterStart: Bool { end > start }
 
     enum InputCombination {
         case startAndEnd, startAndProgress, onlyEnd, onlyProgress
-        // case for ignored combinations such as: all, partial, none
+        // catch all case for ignored combinations such as: all, partial, none
         case ignored
-    }
 
-    func determineCombination() -> InputCombination {
-        let hasStart = !startField.isEmpty
-        let hasEnd = !endField.isEmpty
-        let hasProgress = !progressField.isEmpty
+        static func determine(viewModel: SessionCreatePagesViewModel) -> InputCombination {
+            let hasStart = true, hasEnd = true, hasProgress = true
 
-        switch (hasStart, hasEnd, hasProgress) {
-        case (false, hasEnd, false):
-            return .onlyEnd
-        case (false, false, hasProgress):
-            return .onlyProgress
-        case (hasStart, hasEnd, false):
-            return .startAndEnd
-        case (hasStart, false, hasProgress):
-            return .startAndProgress
-        default:
-            return .ignored
+            switch (viewModel.startIsValid, viewModel.endIsValid, viewModel.progressIsValid) {
+            case (false, hasEnd, false):
+                return .onlyEnd
+            case (false, false, hasProgress):
+                return .onlyProgress
+            case (hasStart, hasEnd, false):
+                return .startAndEnd
+            case (hasStart, false, hasProgress):
+                return .startAndProgress
+            default:
+                return .ignored
+            }
         }
     }
 
-    func attempAutofill() {
-        switch determineCombination() {
+    func autofill() {
+        let combination = InputCombination.determine(viewModel: self)
+
+        switch combination {
         case .startAndEnd:
             progressField = computedProgress
         case .startAndProgress:
@@ -58,8 +60,8 @@ class SessionCreatePagesViewModel: ObservableObject {
         case .onlyProgress:
             startField = computedStart
             endField = computedEnd
-        default:
-            print("all / none / partial - no automation")
+        case .ignored:
+            break
         }
     }
 
@@ -72,7 +74,7 @@ class SessionCreatePagesViewModel: ObservableObject {
         startIsValid && progressIsValid ? String(start + progress) : ""
     }
     var computedProgress: String {
-        startIsValid && endIsValid ? String(end - start) : ""
+        startIsValid && endIsAfterStart ? String(end - start) : ""
     }
 }
 
@@ -88,11 +90,11 @@ struct SessionCreatePagesSection: View {
             TextField("End", text: $viewModel.endField)
                 .keyboardType(.numberPad)
 
-            TextField("Read", text: $viewModel.progressField)
+            TextField("Progress", text: $viewModel.progressField)
                 .keyboardType(.numberPad)
 
             Button("Autofill") {
-                viewModel.attempAutofill()
+                viewModel.autofill()
             }
             Button("Reset") {
                 viewModel.startField = ""
