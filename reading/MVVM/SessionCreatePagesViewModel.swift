797226a7b48be6d2cc6bbf0f6be8a987db7d40a4
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-09-14 19:44:03 +0600

    refactor: new folder structure
    base on: https://github.com/quickbirdstudios/SwiftUI-Architectures/tree/master/QBChat-MVVM/QBChat-MVVM

---
+++ reading/MVVM/SessionCreatePagesViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SessionCreatePagesViewModel.swift
+//  reading
+//
+//  Created by Viktor Khotimchenko on 2020-09-14.
+//  Copyright © 2020 mrv1k. All rights reserved.
+//
+
+import Foundation
