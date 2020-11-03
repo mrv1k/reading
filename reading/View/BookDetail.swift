5027d5441e96ac3762d8c6b894fafe6a8bdc45f2
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-11-02 20:03:25 -0500

    wip: updating completion percent

--- reading/View/BookDetail.swift
+++ reading/View/BookDetail.swift
@@ -1,25 +1,42 @@
 import SwiftUI
 import CoreData
+import Combine
+
+class BookDetailVM: ObservableObject {
+    var cancellableSet = Set<AnyCancellable>()
+    @Published var raw_completionPercent: Int16 = 0
+    // @Published var test: Double = 0
+}
 
 struct BookDetail: View {
+    @StateObject var VM = BookDetailVM()
     let book: Book
 
     var body: some View {
         VStack(alignment: .leading) {
             BookRow(book: book)
 
-
             Text("\(book.raw_completionPercent)")
 
-            // ProgressView(value: proxy, total: 1000)
-            // {}
-            // currentValueLabel: { Text("\(proxy)%") }
+            Text("\(VM.raw_completionPercent)")
+
+            ProgressView(value: Double(book.raw_completionPercent), total: 1000)
+            {}
+            currentValueLabel: { Text("\(book.raw_completionPercent)%") }
+
+            ProgressView(value: Double(VM.raw_completionPercent), total: 1000)
+            {}
+            currentValueLabel: { Text("\(VM.raw_completionPercent)%") }
 
             SessionListBook(book: book)
         }
         .frame(maxHeight: .infinity, alignment: .topLeading)
         .padding([.horizontal, .top], 20)
-        .navigationBarItems(trailing: SimpleSessionCreate(book: book))
+        .onAppear {
+            book.publisher(for: \.raw_completionPercent)
+                .assign(to: \BookDetailVM.raw_completionPercent, on: VM)
+                .store(in: &VM.cancellableSet)
+        }
     }
 }
 
