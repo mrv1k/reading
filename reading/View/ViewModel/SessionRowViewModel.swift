9b6128a57b4885d1bbfd6200a9136afc3541767e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-11-29 18:14:11 -0500

    wip: converting session row to support input

--- reading/View/ViewModel/SessionRowViewModel.swift
+++ reading/View/ViewModel/SessionRowViewModel.swift
@@ -12,53 +12,46 @@ import Foundation
 class SessionRowViewModel: ViewModel, AppSettingsObserver, Identifiable {
     var settings: AppSettings { AppSettings.singleton }
     @Published var session: Session
+    var time = ""
 
     @Published var isInPercents = false
-    private var progressPage = 0
-    private var progressPercent = 0
-    @Published private var progressPageText = ""
-    @Published private var progressPercentText = ""
-    var progressText: String {
-        print("progresstext", isInPercents ? progressPercentText : progressPageText)
-        return isInPercents ? progressPercentText : progressPageText
-    }
+    @Published private var progressPage = 0
+    @Published private var progressPercent = 0
+    private var progressPercentText = ""
+    private var progressPageText = ""
 
-    var time = ""
+    var progressText: String {
+        get { isInPercents ? progressPercentText : progressPageText }
+        set(progress) {
+            if isInPercents {
+                progressPercentText = "\(progress)%"
+            } else {
+                progressPageText = "\(progress) \(progress == "1" ? "page" : "pages")"
+            }
+        }
+    }
 
     var progressBinding: String {
         get { String(isInPercents ? progressPercent : progressPage) }
         set {
             guard let progress = Int(newValue) else { return }
-            print(newValue, "progress", progress)
             if isInPercents {
                 progressPercent = progress
-                progressPercentText = "\(progress)%"
             } else {
                 progressPage = progress
-                progressPageText = "\(progress) page(s)"
             }
+            progressText = String(progress)
         }
     }
 
     init(session: Session) {
         self.session = session
 
+        settings.$sessionIsInPercents.assign(to: &$isInPercents)
         progressPercent = session.progressPercent
         progressPage = Int(session.progressPage)
-
-        settings.$sessionIsInPercents.assign(to: &$isInPercents)
+        progressText = String(isInPercents ? session.progressPercent : Int(session.progressPage))
 
         time = Helpers.dateFormatters.time.string(from: session.createdAt)
-        $session
-            .map(\.progressPage)
-            .print()
-            .map { page in "\(page) \(page == 1 ? "page" : "pages")" }
-            .assign(to: &$progressPageText)
-
-        $session
-            .map(\.progressPercent)
-            .print()
-            .map { "\($0)%" }
-            .assign(to: &$progressPercentText)
     }
 }
