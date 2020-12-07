cc76fa0b6bb8a0de7092938c972ecfeda3cb2d17
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-12-06 22:56:20 -0500

    test: add example unit and UI tests

---
+++ readingTests/readingTests.swift
@@ -0,0 +1,33 @@
+//
+//  readingTests.swift
+//  readingTests
+//
+//  Created by Viktor Khotimchenko on 2020-12-06.
+//  Copyright © 2020 mrv1k. All rights reserved.
+//
+
+import XCTest
+
+class readingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
