94c435ac2419a2e8b07583bc16578b265b10c6c2
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-12-16 18:16:46 -0500

    test: add test for data fetching

--- readingTests/persistence/CDBookStorageTests.swift
+++ readingTests/persistence/CDBookStorageTests.swift
@@ -6,21 +6,48 @@
 //  Copyright © 2020 mrv1k. All rights reserved.
 //
 
+import CoreData
+@testable import reading
 import XCTest
 
+/// Testing `CDBookStorage` and  it's supproting `CDBookSort` 
 class CDBookStorageTests: XCTestCase {
+    var cdBookStorage: CDBookStorage!
+    // Supporting types
+    var persistenceController: PersistenceController!
+    var repository: DomainBookRepository!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        persistenceController = PersistenceController(inMemory: true)
+        let viewContext = persistenceController.container.viewContext
+        repository = DomainBookRepository(context: viewContext)
+        cdBookStorage = CDBookStorage(viewContext: viewContext)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_isEmptyWhenNoBooksAreStored() throws {
+        XCTAssertTrue(cdBookStorage.cdBooks.isEmpty)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_returnsAllStoredBooks() throws {
+        let inputBooks = [DomainBook(title: "titleA", author: "authorA", pageCount: 100),
+                          DomainBook(title: "titleB", author: "authorB", pageCount: 200),
+                          DomainBook(title: "titleC", author: "authorC", pageCount: 300)]
+        inputBooks.forEach { repository.create(domainBook: $0) }
+
+        try! persistenceController.container.viewContext.save()
+
+        XCTAssert(cdBookStorage.cdBooks.count == inputBooks.count, "Expected all persited books to be returned")
     }
 
+    func test_loadsLastBookSort() {
+        // given
+        // when
+        // then
+    }
+
+    func test_usesTitleSortWhenNoSavedSortIsFound() {
+        // given
+        // when
+        // then
+    }
 }
