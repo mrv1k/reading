b292e35ae49c5bc0f4891c6a9a03eaa62765b372
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-12-16 18:18:14 -0500

    test: rename `expected` to `input`

--- readingTests/domain/DomainBookRepositoryTests.swift
+++ readingTests/domain/DomainBookRepositoryTests.swift
@@ -12,20 +12,20 @@ import XCTest
 
 class DomainBookRepositoryTests: XCTestCase {
     var persistenceController: PersistenceController!
-    var viewContext: NSManagedObjectContext!
     var repository: DomainBookRepository!
 
     override func setUp() {
         persistenceController = PersistenceController(inMemory: true)
-        viewContext = persistenceController.container.viewContext
+        let viewContext = persistenceController.container.viewContext
         repository = DomainBookRepository(context: viewContext)
     }
 
     func test_shouldCreateABook() throws {
-        let expected = DomainBook(title: "title", author: "author", pageCount: 100)
-        let result = repository.create(domainBook: expected)
+        let input = DomainBook(title: "title", author: "author", pageCount: 100)
 
-        XCTAssertTrue(result == expected, "Saves properties without mutations")
+        let result = repository.create(domainBook: input)
+
+        XCTAssertTrue(result == input, "Saves properties without mutations")
         XCTAssertNotNil(result.persistenceID, "Set `persistenceID` as indicator of getting persisted")
     }
 
@@ -34,6 +34,7 @@ class DomainBookRepositoryTests: XCTestCase {
         let created = repository.create(domainBook: input)
 
         let result = repository.get(id: created.persistenceID)
+
         XCTAssertNotNil(result)
         XCTAssertNoThrow(result, "Expected to handle errors")
     }
@@ -42,7 +43,6 @@ class DomainBookRepositoryTests: XCTestCase {
         let inputBooks = [DomainBook(title: "titleA", author: "authorA", pageCount: 100),
                           DomainBook(title: "titleB", author: "authorB", pageCount: 200),
                           DomainBook(title: "titleC", author: "authorC", pageCount: 300)]
-
         inputBooks.forEach { repository.create(domainBook: $0) }
 
         let result = repository.getAll()
