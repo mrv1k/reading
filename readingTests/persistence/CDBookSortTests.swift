eefcf7d5a8c3bb3baaba2596bb2c39d34e603cf8
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-12-16 23:57:33 -0500

    test: coverage for CDBookSort

--- readingTests/persistence/CDBookSortTests.swift
+++ readingTests/persistence/CDBookSortTests.swift
@@ -6,28 +6,74 @@
 //  Copyright © 2020 mrv1k. All rights reserved.
 //
 
+@testable import reading
 import XCTest
 
 class CDBookSortTests: XCTestCase {
+    var factory: CDBookSort.Factory!
+    // helpers
+    private var userDefaults: UserDefaults!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        userDefaults = UserDefaults(suiteName: #file)
+        userDefaults.removePersistentDomain(forName: #file)
+        factory = CDBookSort.Factory(userDefaults: userDefaults)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_createsSortFromSelection() {
+        let result = factory.create(selection: .author)
+        XCTAssert(result.selection == .author)
+        XCTAssert(result.isAscending == false)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_createsReversedSortFromSelection() {
+        let sortByAuthorAscending = factory.create(selection: .author, ascending: true)
+        XCTAssert(sortByAuthorAscending.selection == .author)
+        XCTAssert(sortByAuthorAscending.isAscending == true)
+
+        let sortByAuthor = factory.create(selection: .author, ascending: false)
+        XCTAssert(sortByAuthor.selection == .author)
+        XCTAssert(sortByAuthor.isAscending == false)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_correctlyMapsSelectionToSort() {
+        let selectedSortAuthor = factory.create(selection: .author)
+        let selectedSortCreatedAt = factory.create(selection: .createdAt)
+        let selectedSortTitle = factory.create(selection: .title)
+
+        XCTAssert(selectedSortAuthor.selection == .author)
+        XCTAssert(selectedSortCreatedAt.selection == .createdAt)
+        XCTAssert(selectedSortTitle.selection == .title)
     }
 
+    func test_savesSort() {
+        let initiallySavedSort = factory.loadSort()
+        let sortByAuthor = factory.create(selection: .author)
+
+        let currentlySaved = factory.loadSort()
+
+        XCTAssert(initiallySavedSort.selection != .author)
+        XCTAssert(currentlySaved.selection == sortByAuthor.selection)
+    }
+
+    func test_loadsSort() {
+        let sortByAuthor = factory.create(selection: .author)
+        let result = factory.loadSort()
+        XCTAssert(result.selection == sortByAuthor.selection)
+    }
+
+    func test_bugfix_loadedSortDoesNotGetSaved() {
+        let initiallySavedSort = userDefaults.string(forKey: UserDefaultsKey.cdBookSort.rawValue)
+        XCTAssertNil(initiallySavedSort)
+
+        _ = factory.loadSort()
+
+        let savedSort = userDefaults.string(forKey: UserDefaultsKey.cdBookSort.rawValue)
+        XCTAssertNil(savedSort)
+    }
+
+    func test_defaultsToTitleSortWhenSavedSortIsNotFound() {
+        let result = factory.loadSort()
+        XCTAssert(result.selection == .title)
+    }
 }
