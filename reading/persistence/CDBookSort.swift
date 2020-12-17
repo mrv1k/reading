137d8e8a15ebf737eac600237ec9150efb6a928d
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-12-16 23:38:29 -0500

    refactor: update to support testing

--- reading/persistence/CDBookSort.swift
+++ reading/persistence/CDBookSort.swift
@@ -32,7 +32,11 @@ struct CDBookSort {
 
     // TODO: would be a smart idea to just convert Factory to a Manager and use that instead of saving on every change
     class Factory {
-        fileprivate init() {}
+        private var userDefaults: UserDefaults
+
+        init(userDefaults: UserDefaults = .standard) {
+            self.userDefaults = userDefaults
+        }
 
         func create(selection: Selection, ascending: Bool? = nil, save: Bool = true) -> Sort {
             let isAscending = ascending != nil ? ascending! : loadAscending(selection: selection)
@@ -51,27 +55,27 @@ struct CDBookSort {
             return sort
         }
 
-        private func saveSelection(selection: Selection) {
-            UserDefaults.standard.set(selection.rawValue, forKey: UserDefaultsKey.cdBookSort.rawValue)
-        }
-
-        fileprivate func loadSelection() -> Selection {
-            guard let rawSelection = UserDefaults.standard.string(forKey: UserDefaultsKey.cdBookSort.rawValue)
-            else { return .title }
-            return Selection(rawValue: rawSelection)!
-        }
-
-        fileprivate func loadSort() -> Sort {
+        func loadSort() -> Sort {
             let selection = loadSelection()
             return create(selection: selection, save: false)
         }
 
+        private func saveSelection(selection: Selection) {
+            userDefaults.set(selection.rawValue, forKey: UserDefaultsKey.cdBookSort.rawValue)
+        }
+
+        private func loadSelection() -> Selection {
+            guard let rawSelection = userDefaults.string(forKey: UserDefaultsKey.cdBookSort.rawValue)
+            else { return .title }
+            return Selection(rawValue: rawSelection)!
+        }
+
         private func saveAscending(_ value: Bool, selection: Selection) {
-            UserDefaults.standard.set(value, forKey: selection.ascendingKey.rawValue)
+            userDefaults.set(value, forKey: selection.ascendingKey.rawValue)
         }
 
         private func loadAscending(selection: Selection) -> Bool {
-            UserDefaults.standard.bool(forKey: selection.ascendingKey.rawValue)
+            userDefaults.bool(forKey: selection.ascendingKey.rawValue)
         }
     }
 }
@@ -84,7 +88,9 @@ protocol CDBookSortProtocol {
 
 extension CDBookSort {
     typealias Sort = CDBookSortProtocol
+}
 
+private extension CDBookSort {
     struct ByTitle: Sort {
         let selection: Selection = .title
         var isAscending: Bool
