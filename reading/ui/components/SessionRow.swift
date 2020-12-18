f8d214bf872a845c1d26e9ac0fa4b3a0a759d439
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2020-12-17 21:37:04 -0500

    wip: progress on domain session and session row

--- reading/ui/components/SessionRow.swift
+++ reading/ui/components/SessionRow.swift
@@ -8,32 +8,52 @@
 
 import SwiftUI
 
-struct SessionRow: View, ViewModelObserver {
-    @ObservedObject var viewModel: SessionRowViewModel
+struct DomainSession: Equatable, Identifiable {
+    var id: UUID { persistenceID ?? UUID() }
+    var persistenceID: UUID? = nil
+
+    var percent: String = "10%"
+    var page: String = "10"
+    var recordedTime: String = Helpers.dateFormatters.time.string(from: Date())
+//    time = Helpers.dateFormatters.time.string(from: session.createdAt)
+}
+
+struct SessionRow: View {
+//    @ObservedObject var viewModel: SessionRowViewModel
+    let session = DomainSession()
 
     var body: some View {
         HStack(spacing: 0) {
-            Text(viewModel.progress)
-            Text(viewModel.progressTrailingText).foregroundColor(.gray)
+            Text(session.page)
+            Text(" pages").foregroundColor(.gray)
 
             Spacer()
-            Text(viewModel.time).font(.caption).foregroundColor(.gray)
+            Text(session.recordedTime).font(.caption).foregroundColor(.gray)
         }
     }
 }
 
 struct SessionRow_Previews: PreviewProvider {
     static var previews: some View {
-        let book = BookSeeder.preview.fetch(bookWith: .sessions)
-        let session = book.sessions.first!
-        let viewModel = SessionRowViewModel(session: session)
-
+//        let session = DomainSession()
+//        createdAt: Date(), progressPage: 13, raw_progressPercent: 130
         return Group {
-            SessionRow(viewModel: viewModel)
+            SessionRow()
+                .previewLayout(.sizeThatFits)
 
-//            SessionRow(
-//                viewModel: SessionRowViewModel(createdAt: Date(), progressPage: 13, raw_progressPercent: 130)
-//            )
-        }.previewLayout(.sizeThatFits)
+            List {
+                Section(header: Text("Today")) {
+                    SessionRow()
+                }
+                Section(header: Text("December 16")) {
+                    SessionRow()
+                }
+                Section(header: Text("December 15")) {
+                    SessionRow()
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .previewDevice("iPhone SE (2nd generation)")
+        }
     }
 }
